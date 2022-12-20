a5bb1e2394b0df9ebb6f1802215701f36cd14078
axelcatasus <axel.catasus@gmail.com>
2022-12-20 12:08:01 +0100

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `Portfolio`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-contentful',
+    options: {
+      "accessToken": "ql7DZ_-loYRgthXRFo_6BTs_yXx2Zeg4tvc1tBx1GSY",
+      "spaceId": "dphdxqy6qqbr"
+    }
+  }, "gatsby-plugin-image", "gatsby-plugin-sharp", "gatsby-transformer-sharp", "gatsby-plugin-sass", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
